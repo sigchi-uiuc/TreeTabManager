673461100fd4023d3e881f778f04b241d38c2dac
Jeffrey <jeffrey.my.tsai@gmail.com>
2017-02-02 19:39:52 -0600

    Alert Boxes created for opening a new tab, closing a tab, detaching a tab, refresh window

--- js/event.js
+++ js/event.js
@@ -3,22 +3,33 @@
 console.log("Helo");
 //new tab listener
 chrome.tabs.onCreated.addListener(function(tab){
-	alert("Hello! I am an alert box!!");
+	alert("New Tab Created");
 });
 
 //tab removed listener
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
-	
+	alert("Tab deleted");
 });
 
 //tab updated listener
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+	// alert("Tab was updated"); 
 	
+	/*This alert box pops up 4 times 
+	when you go to a new webpage, not sure why, maybe this is 
+	just how chrome is programmed.
+	*/
+
+	/* Update:
+		I have a hunch that it depends on how a certain page is programmed.
+		For example, loading webpage will allow the alert box to pop up 4 times
+		It alerts immediately to right when it loads, then three more times. 
+		I'm assuming it's alerting as files properly load (css, scripts, etc)
+	*/
 });
 //tab detached listener
 chrome.tabs.onDetached.addListener(function(tabId,detachInfo){
-
-
+	alert("Tab was detached"); //Alert box pops up once 
 });
 //tab attached listener
 chrome.tabs.onAttached.addListener(function(tabId,detachInfo){
