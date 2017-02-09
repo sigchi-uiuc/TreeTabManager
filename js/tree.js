e09aedce76d71b252e7628c25ad956369b382017
akuznets0v <akuz.dev@gmail.com>
2017-02-08 19:09:18 -0600

    Beginning of tree.js

---
+++ js/tree.js
@@ -0,0 +1,21 @@
+//Lifted from https://code.tutsplus.com/articles/data-structures-with-javascript-tree--cms-23393
+
+function Node(data) {
+    this.data = data;
+    this.parent = null;
+    this.children = [];
+}
+
+function Tree(data) {
+    var node = new Node(data);
+    this._root = node;
+}
+
+var tree = new Tree('Base');
+
+tree._root;
+
+
+
+
+
