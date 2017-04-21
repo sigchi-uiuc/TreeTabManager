63131062b3e3dc6db9e8c77ae5e478a05287d95b
Jeffrey <jeffrey.my.tsai@gmail.com>
2017-04-20 19:43:37 -0500

    April 20th Commit

--- js/tree.js
+++ js/tree.js
@@ -8,7 +8,7 @@ function Node(alive, tab_id, url, title, favicon_url, parent, children) {
     this._parent = parent;
     this._children = children;
 }
-function Tree(data) {
+function Tree() {
     var node = new Node(true, -1, null, null, null, null, []);
     this._root = node;
 }
@@ -25,7 +25,19 @@ function deleteSelfAndChildren(node){
         node._parent._children.splice(index,1);
     }
 }
+function AddChild(parent,child) {
+    (parent._children).push(child);
+    child._parent=parent;
+}
+function Traversal(node){
+    console.log(node._title);
+    for(var i=0;i<node._children.length;i++){
+        Traversal(node._children[i]);
+    }
 
+}
+var run = "hello";
 
+// var roots = { 0:targetJson};
 
 
--- js/tree_view.js
+++ js/tree_view.js
@@ -1,7 +1,91 @@
-$(document).ready(function(){
-	$('.title').click(function(){
-		$(".title").css({"color": "red"});
-		d3.selectAll("p").style("color", "white");
-	});
+$(document).ready(function() {
+    $('.title').click(function() {
+        $(".title").css({ "color": "red" });
+        d3.selectAll("p").style("color", "white").fadeIn(300);
+    });
+    var tree = new Tree();
+    // alert((tree._root)._alive);
+    var node1 = new Node(
+        true,
+        234976,
+        "hello.com",
+        "hellothere",
+        "hello.com/favicon.jpg",
+        null, []
+    );
+
+    var node2 = new Node(
+        true,
+        23423,
+        "hey.com",
+        "heythere",
+        "hey.com/favicon.jpg",
+        null, []
+    );
+    // console.log(node1._title);
+    AddChild(tree._root, node1);
+    AddChild(node1, node2);
+    // console.log(hello);
+    // console.log
+
+
+    /******* TESTING JSON AND D3 *********/
+
+
+    var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height"),
+    g = svg.append("g").attr("transform", "translate(60,0)");
+
+var tree = d3.cluster()
+    .size([height, width - 160]);
+
+var stratify = d3.stratify()
+    .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
+
+d3.json("treeData.json", function(error, treeData) {
+  if (error) throw error;
+
+  var root = d3.hierarchy(treeData);
+  tree(root);
+
+  var link = g.selectAll(".link")
+      .data(root.descendants().slice(1))
+    .enter().append("path")
+      .attr("class", "link")
+      .attr("d", function(d) {
+        return "M" + d.y + "," + d.x
+            + "C" + (d.parent.y + 100) + "," + d.x
+            + " " + (d.parent.y + 100) + "," + d.parent.x
+            + " " + d.parent.y + "," + d.parent.x;
+      });
+
+  var node = g.selectAll(".node")
+      .data(root.descendants())
+    .enter().append("g")
+      .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+      .attr("transform", function(d) { 
+        return "translate(" + d.y + "," + d.x + ")"; 
+      })
+
+  node.append("circle")
+      .attr("r", 2.5);
+
+  node.append("text")
+      .attr("dy", 3)
+      .attr("x", function(d) { return d.children ? -8 : 8; })
+      .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+      .text(function(d) { 
+        return d.data.name;
+      });
+});
+
+
+
+
+
+
+
+
 
 });
