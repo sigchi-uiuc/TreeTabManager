57aa260d5a753e210076195084758432d9b53c9d
Jeffrey Tsai <jeffreytsai@wirelessprv-10-195-97-18.near.illinois.edu>
2018-03-31 17:33:36 -0500

    Re-modeled Code

--- js/event.js
+++ js/event.js
@@ -1,12 +1,33 @@
 chrome.tabs.onCreated.addListener(function(tab){
 	// alert("Tab created");
-	var tab = new Node(tab.title, tab.id, tab.url, tab.openerTabId);
-	
+	alert("New Tab Created");
+	// Node.addNode(temp);
+    // count++;
+    if (tab.openerTabId == null) {
+    	// rootNode = new Node ("test", count, "test.com", count+4);
+        rootNode = new Node(tab.title, tab.id, tab.url, tab.openerTabId);
+        // alert("New Window");
+        // console.log(rootNode);
+    	treeData.push(rootNode);
+        // console.log(treeData);
+    	alert("TreeData Pushed to");
+    } 
+    else {
+            var temp = new Node(tab.title, tab.id, tab.url, tab.openerTabId);
+            addChild(tab.openerTabId, temp);
+    }
+    var myJson = JSON.stringify(treeData);
+    console.log(myJson);
+    localStorage.setItem("rootNode",myJson);
+    console.log(localStorage.getItem("rootNode"));
 });
+function handleCreation(tab){
+
+}
 
 // //tab removed listener
 function handleRemoved(tabId, removeInfo) {
-  alert("Tab: " + tabId + " is closing \n Window ID: " + removeInfo.windowId + "\nWindow is closing: " + removeInfo.isWindowClosing);
+  // alert("Tab: " + tabId + " is closing \n Window ID: " + removeInfo.windowId + "\nWindow is closing: " + removeInfo.isWindowClosing);
 }
 
 chrome.tabs.onRemoved.addListener(handleRemoved);
@@ -17,7 +38,7 @@ chrome.tabs.onRemoved.addListener(handleRemoved);
 
 function handleUpdated(tabId, changeInfo) {
   // alert("Tab: " + tabId + " is updated \n Status: " 
-  // 	+ changeInfo.status + "\nTitle: " + changeInfo.title + "\n URL: " + changeInfo.url );
+  	// + changeInfo.status + "\nTitle: " + changeInfo.title + "\n URL: " + changeInfo.url );
 }
 chrome.tabs.onUpdated.addListener(handleUpdated);
 
---
+++ js/tree_view.js
@@ -0,0 +1,195 @@
+$(document).ready(function() {
+    $('.title').click(function() {
+        $(".title").css({ "color": "red" });
+        d3.selectAll("p").style("color", "white").fadeIn(300);
+    });
+    // var treeData = document.getElementById("main");
+
+    /******* TESTING JSON AND D3 *********/
+    // var tree1 = JSON.stringify(tree._root)
+    // var treeData = JSON.parse(tree1); 
+    // console.log(tree1);
+
+    // Traversal(tree._root);
+
+    // var visualize = function(data){
+    /***** TEST ******/
+
+var treeData4 = [
+  {
+    "name": "Root Node",
+    "parent": "null",
+    "children": [
+      {
+        "name": "Level 2: A",
+        "parent": "Top Level",
+        "children": [
+          {
+            "name": "Son of A",
+            "parent": "Level 2: A"
+          },
+          {
+            "name": "Daughter of A",
+            "parent": "Level 2: A"
+          }
+        ]
+      },
+      {
+        "name": "Level 2: B",
+        "parent": "Top Level"
+      }
+    ]
+  }
+];
+  var treeData2 = localStorage.getItem("rootNode");
+  console.log(treeData2);
+  var treeData = JSON.parse(treeData2);
+  console.log(treeData);
+  var margin = {top: 20, right: 120, bottom: 20, left: 120},
+    width = 800- margin.right - margin.left,
+    height = 500 - margin.top - margin.bottom;
+
+  var svg = d3.select("#body")
+          .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom);
+
+  var i = 0,
+    duration = 750,
+    root;
+
+  var tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .offset([10, 0])
+            .direction("s")
+            .html(function(d) {
+                return d.name;
+            });
+
+  svg.call(tip);
+
+  var tree = d3.layout.tree()
+    .size([height, width]);
+
+  var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.y, d.x]; });
+
+
+  root = treeData[0];
+  root.x0 = 200;
+  root.y0 = 100;
+
+  console.log(treeData);
+
+  update(root);
+   
+  d3.select(self.frameElement).style("height", "500px");
+
+  function update(source) {
+
+    // Compute the new tree layout.
+    var nodes = tree.nodes(root).reverse(),
+      links = tree.links(nodes);
+
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+    // Update the nodes…
+    var node = svg.selectAll("g.node")
+      .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+    // Enter any new nodes at the parent's previous position.
+    var nodeEnter = node.enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+      .on("click", click);
+
+    nodeEnter.append("circle")
+      .attr("r", 1e-6)
+      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; })
+      .style("transform", "translate(200px,0px")
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+
+    nodeEnter.append("text")
+      .attr("x", function(d) { return d.children || d._children ? -13 : 13; })
+      .attr("dy", ".35em")
+      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+      .text(function(d) { return d._title; })
+      .style("transform", "translate(200px,0px")
+      .style("fill-opacity", 1e-6);
+
+    // Transition nodes to their new position.
+    var nodeUpdate = node.transition()
+      .duration(duration)
+      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+    nodeUpdate.select("circle")
+      .attr("r", 10)
+      .style("fill", function(d) { return "black"});
+
+    nodeUpdate.select("text")
+      .style("fill-opacity", 1);
+
+    // Transition exiting nodes to the parent's new position.
+    var nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+      .remove();
+
+    nodeExit.select("circle")
+      .attr("r", 1e-6);
+
+    nodeExit.select("text")
+      .style("fill-opacity", 1e-6);
+
+    // Update the links…
+    var link = svg.selectAll("path.link")
+      .style("transform", "translate(200px,0px")
+      .data(links, function(d) { return d.target.id; });
+
+    // Enter any new links at the parent's previous position.
+    link.enter().insert("path", "g")
+      .attr("class", "link")
+      .style("transform", "translate(200px,0px")
+      .attr("d", function(d) {
+      var o = {x: source.x0, y: source.y0};
+      return diagonal({source: o, target: o});
+      });
+
+    // Transition links to their new position.
+    link.transition()
+      .duration(duration)
+      .attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition()
+      .duration(duration)
+      .attr("d", function(d) {
+      var o = {x: source.x, y: source.y};
+      return diagonal({source: o, target: o});
+      })
+      .remove();
+
+    // Stash the old positions for transition.
+    nodes.forEach(function(d) {
+    d.x0 = d.x;
+    d.y0 = d.y;
+    });
+  }
+
+  // Toggle children on click.
+  function click(d) {
+    if (d.children) {
+    d._children = d.children;
+    d.children = null;
+    } else {
+    d.children = d._children;
+    d._children = null;
+    }
+    update(d);
+  }
+// }
+
+});
+
